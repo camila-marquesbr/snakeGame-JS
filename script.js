653f41e60a3bcabceb8bcbade91380ee1a9e6598
camila-marquesbr <camiladev.br@gmail.com>
2020-09-22 11:47:37 -0300

    criando a cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+
+//Criação do canvas
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 
+//Criando a cobrinha
+let snake = [];
+snake[0]= {
+    x: 8 * box,
+    y: 8 * box
+}
+
+//Criação do canvas
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
+//Criando a cobrinha
+function criarCobrinha(){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green"; // uma cor verde
+        context.fillRect(snake[i].x, snake[i].y, box, box); // o tamanho da cobrinha
+    }
+}
 
 criarBG();
+criarCobrinha()
